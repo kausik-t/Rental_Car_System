5c9dfc7443053c97aac17a35879caa6504a2e646
Kausik T <146412835+kausik-t@users.noreply.github.com>
2024-12-16 21:32:38 +0530

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/rental_car_system";
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "password"; 
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try (Connection conn = getConnection()) {
+            if (conn != null) {
+                System.out.println("Connection successful!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
